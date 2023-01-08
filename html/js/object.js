ed28b68adca99a906bd11859e7df34cc5c66375f
sathiyaseelan <sathiyaseelan2598@gmail.com>
2023-01-08 12:07:37 +0530

    JS object and array

---
+++ html/js/object.js
@@ -0,0 +1,12 @@
+var arr=["a","b","c"];
+var info={
+    name:"dhoni",
+    age:40,
+    country:"india",
+    fun:demo(),
+    ar:arr
+}
+function demo(){
+    console.log("i am cricketer");
+}
+console.log(info.ar[0]);
