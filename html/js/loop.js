e45dbd4f549fac32d1ef86da560f48cea1754850
sathiyaseelan <sathiyaseelan2598@gmail.com>
2023-01-09 18:59:29 +0530

    JS loop

---
+++ html/js/loop.js
@@ -0,0 +1,18 @@
+console.log("-------for--------");
+for(i=0;i<10;i++){
+    console.log(i);
+}
+
+console.log("-------while--------");
+var i=0;
+while(i<10){
+    console.log(i);
+    i++;
+}
+console.log("-------do while--------");
+var j=0;
+do{
+    console.log(j);
+    j++;
+}
+while(j<10);
