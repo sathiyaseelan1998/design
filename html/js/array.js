5533600ce0bf553224413c6fa75306ae23423263
sathiyaseelan <sathiyaseelan2598@gmail.com>
2023-01-22 07:29:15 +0530

    Array & create html tag in javascript

--- html/js/array.js
+++ html/js/array.js
@@ -2,3 +2,11 @@ var ar=["one","two","three"];
 for(i=0;i<ar.length;i++){
     console.log(ar[i]);
 }
+ar.push("four");//add last element
+console.log(ar);
+ar.pop(3);//remove last element
+console.log(ar);
+ar.shift(1);//remove first element
+console.log(ar);
+ar.unshift("ONE");//add first element
+console.log(ar);
---
+++ html/js/create.js
@@ -0,0 +1,4 @@
+let headingTag=document.createElement("h1");
+let val=document.createTextNode("welcome to JS");
+headingTag.appendChild(val);
+document.body.appendChild(headingTag);
