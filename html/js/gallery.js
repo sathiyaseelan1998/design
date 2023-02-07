8857d5bd81a3c8c84ca3c3e2db04eba1c7dd1893
sathiyaseelan <sathiyaseelan2598@gmail.com>
2023-02-07 16:06:18 +0530

    gallery

--- html/js/gallery.js
+++ html/js/gallery.js
@@ -1,2 +1,13 @@
-const image=document.createElement('div');
-document.body.appendChild(image);
+const blur=document.createElement('div');
+document.body.appendChild(blur);
+blur.id='blur';
+
+const images=document.querySelectorAll('.img');
+
+images.forEach(image=>{
+    image.addEventListener('click',()=>
+    alert("hi")
+    )
+}
+)
+
