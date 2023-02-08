285c9f40027ef2cf8e46136844cc48294b60448e
sathiyaseelan <sathiyaseelan2598@gmail.com>
2023-02-08 18:59:29 +0530

    gallery

--- html/js/gallery.js
+++ html/js/gallery.js
@@ -5,9 +5,16 @@ blur.id='blur';
 const images=document.querySelectorAll('.img');
 
 images.forEach(image=>{
-    image.addEventListener('click',()=>
-    alert("hi")
-    )
-}
-)
+    image.addEventListener('click',()=>{
+        blur.classList.add('active');
+        const img=document.createElement('img');
+        blur.appendChild(img);
+        img.src=image.src;
+        img.id='img';
+
+    });
+});
+blur.addEventListener('click',()=>{
+    blur.classList.remove('active');
+});
 
